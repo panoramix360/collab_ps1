ff0121b7be837bb892d0c6c8d4dd3938e5da7905
Lucas Reis <lucasreis3000@gmail.com>
2020-06-11 19:28:31 -0300

    ps1 intro

---
+++ CollabPs1/Assets/Scripts/VideoController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoController : MonoBehaviour
+{
+    private VideoPlayer videoPlayer;
+
+    private void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        videoPlayer.loopPointReached += OnEnd;
+    }
+
+    private void OnEnd(VideoPlayer vp)
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
